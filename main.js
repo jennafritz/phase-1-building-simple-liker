9a827b6de89f36dbb7cfcb9d4aec77450bf73102
jennafritz <jennafritz24@gmail.com>
2021-04-12 20:35:57 -0400

    Complete

--- main.js
+++ main.js
@@ -4,8 +4,31 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let hearts = document.querySelectorAll(".like-glyph")
+let errorDiv = document.querySelector("#modal")
+let errorP = document.querySelector("#modal-message")
 
-
+for (const heart of hearts) {
+  heart.addEventListener("click", function(){
+    if (heart.innerText === FULL_HEART){
+      heart.innerText = EMPTY_HEART
+      heart.classList.remove("activated-heart")
+    } else if (heart.innerText === EMPTY_HEART){
+      mimicServerCall()
+      .then(() => {
+        heart.innerText = FULL_HEART
+        heart.className = "activated-heart"
+      })
+      .catch((error) => {
+        errorDiv.className = ""
+        errorP.innerText = error
+        setTimeout(function(){
+          errorDiv.className = "hidden"
+        }, 3000)
+      })
+    }
+  })
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
